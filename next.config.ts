2cd75fcd2d2d2f5a2cda4c1dd545f9de4843202d
Alejandro Marañés Escribano <03218328F@csis.dmn>
2025-03-13 09:35:20 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,19 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
+export default {
+  experimental: {
+    ppr: true,
+    inlineCss: true,
+    useCache: true,
+    reactOwnerStack: true,
+    newDevOverlay: true
+  },
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.shopify.com',
+        pathname: '/s/files/**'
+      }
+    ]
+  }
 };
-
-export default nextConfig;
